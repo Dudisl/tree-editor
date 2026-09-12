b735057ed6221fcfb6e22545b0a13cd1ef8b8703
Dudisl <207031068+Dudisl@users.noreply.github.com>
2026-09-12 18:49:15 +0300

    Use styled Tree Editor provider for composable components

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,15 +1,13 @@
 export { default as TreeEditorApp } from "./StableTreeEditorApp";
 export type { TreeEditorAppProps } from "./StableTreeEditorApp";
+export { TreeEditorProvider } from "./TreeEditorProvider";
 export {
-  TreeEditorProvider,
   TreeEditorHeader,
   TreeEditorTree,
   TreeEditorNodeForm,
 } from "./TreeEditorComponents";
-export type {
-  TreeEditorProviderProps,
-  TreeEditorTreeProps,
-} from "./TreeEditorComponents";
+export type { TreeEditorProviderProps } from "./TreeEditorProvider";
+export type { TreeEditorTreeProps } from "./TreeEditorComponents";
 export type {
   TreeEditorContext,
   TreeEditorUiConfig,
